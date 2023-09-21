40c9d9c9b32a9c1d2f0e9929b4061c6957726bfe
Andrew Snodgrass <git@pointlander.info>
2023-09-20 22:36:14 -0500

    Machian model

--- go.mod
+++ go.mod
@@ -3,7 +3,19 @@ module github.com/pointlander/mach
 go 1.18
 
 require (
-	github.com/pointlander/pagerank v0.0.0-20210619221740-830548a59275 // indirect
-	github.com/ziutek/blas v0.0.0-20190227122918-da4ca23e90bb // indirect
-	gonum.org/v1/gonum v0.14.0 // indirect
+	github.com/pointlander/pagerank v0.0.0-20210619221740-830548a59275
+	github.com/ziutek/blas v0.0.0-20190227122918-da4ca23e90bb
+	gonum.org/v1/gonum v0.14.0
+	gonum.org/v1/plot v0.10.1
+)
+
+require (
+	git.sr.ht/~sbinet/gg v0.3.1 // indirect
+	github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b // indirect
+	github.com/go-fonts/liberation v0.3.0 // indirect
+	github.com/go-latex/latex v0.0.0-20230307184459-12ec69307ad9 // indirect
+	github.com/go-pdf/fpdf v0.6.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/image v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,5 +4,105 @@
 
 package main
 
+import (
+	"image"
+	"image/color"
+	"image/color/palette"
+	drw "image/draw"
+	"image/gif"
+	"math"
+	"os"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
+	"gonum.org/v1/plot/vg/vgimg"
+)
+
+// Particle is a particle
+type Particle struct {
+	X, Y float64
+}
+
 func main() {
+	particles := []Particle{
+		{X: 0, Y: 0},
+		{X: 128, Y: 0},
+		{X: 0, Y: 128},
+	}
+
+	getEntropy := func() []float64 {
+		distances := NewMatrix(0, len(particles), len(particles))
+		for _, a := range particles {
+			for _, b := range particles {
+				d := math.Sqrt(math.Pow(a.X-b.X, 2) + math.Pow(a.Y-b.Y, 2))
+				distances.Data = append(distances.Data, d)
+			}
+		}
+		units := Normalize(distances)
+		embedding := SelfEntropy(units, units, units)
+		return embedding
+	}
+	images := make([]*image.Paletted, 100)
+	for s := 0; s < 100; s++ {
+		points := make(plotter.XYs, 0, len(particles))
+		for i := range particles {
+			particle, min, x, y := particles[i], math.MaxFloat64, 0.0, 0.0
+			for j := -1; j <= 1; j++ {
+				for k := -1; k <= 1; k++ {
+					particles[i].X += float64(j)
+					particles[i].Y += float64(k)
+					entropy := getEntropy()
+					if e := -entropy[i]; e < min {
+						min, x, y = e, particles[i].X, particles[i].Y
+					}
+					particles[i] = particle
+				}
+			}
+			particles[i].X, particles[i].Y = x, y
+			points = append(points, plotter.XY{X: x, Y: y})
+		}
+
+		p := plot.New()
+
+		p.Title.Text = "x vs y"
+		p.X.Label.Text = "x"
+		p.Y.Label.Text = "y"
+
+		scatter, err := plotter.NewScatter(points)
+		if err != nil {
+			panic(err)
+		}
+		scatter.GlyphStyle.Radius = vg.Length(3)
+		scatter.GlyphStyle.Shape = draw.CircleGlyph{}
+		p.Add(scatter)
+
+		c := vgimg.New(8*vg.Inch, 8*vg.Inch)
+		p.Draw(draw.New(c))
+
+		opts := gif.Options{
+			NumColors: 256,
+			Drawer:    drw.FloydSteinberg,
+		}
+		bounds := c.Image().Bounds()
+
+		paletted := image.NewPaletted(bounds, palette.Plan9[:opts.NumColors])
+		if opts.Quantizer != nil {
+			paletted.Palette = opts.Quantizer.Quantize(make(color.Palette, 0, opts.NumColors), c.Image())
+		}
+		opts.Drawer.Draw(paletted, bounds, c.Image(), image.Point{})
+
+		images[s] = paletted
+	}
+
+	animation := &gif.GIF{}
+	for _, paletted := range images {
+		animation.Image = append(animation.Image, paletted)
+		animation.Delay = append(animation.Delay, 0)
+	}
+
+	f, _ := os.OpenFile("animation.gif", os.O_WRONLY|os.O_CREATE, 0600)
+	defer f.Close()
+	gif.EncodeAll(f, animation)
 }
